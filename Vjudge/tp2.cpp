71b3e9cad06d3141871b4191b742776bf97f3f6a
Tievoo <tievolib@gmail.com>
2024-04-11 01:42:41 -0300

    protools

--- Vjudge/tp2.cpp
+++ Vjudge/tp2.cpp
@@ -1,30 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
 
+int lds(vector<int>& h, vector<int>& w, int lng) {
+    vector<int> m(lng, -1);
+    int maxD = 0;
+    for (int i = 0; i < lng; i++) {
+        m[i] = w[i];
+        for (int j = 0; j < i; j++) {
+            if (h[i] < h[j]) {
+                m[i] = max(m[i], w[i] + m[j]);
+            }
+        }
+        maxD = max(m[i], maxD);
+    }
+    
+    return maxD;
+}
+
+int lis(vector<int>& h, vector<int>& w, int lng) {
+    vector<int> m(lng, -1);
+    int maxI = 0;
+    for (int i = 0; i < lng; i++) {
+        m[i] = w[i];
+        for (int j = 0; j < i; j++) {
+            if (h[i] > h[j]) {
+                m[i] = max(m[i], w[i] + m[j]);
+            }
+        }
+        maxI = max(m[i], maxI);
+    }
+    return maxI;
+}
+
 int main() {
-    int casos;
-    cin >> casos;
+    int n;
+    cin >> n;
 
-    for (int caso = 1; caso <= casos; caso++) {
-        int n;
-        cin >> n;
+    for (int k = 1; k <= n; k++) {
+        int lng;
+        cin >> lng;
         
-        vector<int> altos(n);
-        vector<int> anchos(n);
+        vector<int> h(lng);
+        vector<int> w(lng);
         
-        for (int i = 0; i < n; i++) {
-            cin >> altos[i];
-            printf("elemento %d", altos[i]);
+        for (int i = 0; i < lng; i++) {
+            cin >> h[i];
         }
         
-        for (int i = 0; i < n; i++) {
-            cin >> anchos[i];
-            printf("elemento %d", anchos[i]);
+        for (int i = 0; i < lng; i++) {
+            cin >> w[i];
         }
-
-        printf("Case %d: ", caso);
-        printf("%d\n", n);
+        
+        int inc = lis(h, w, lng);
+        int dec = lds(h, w, lng);
+        
+        string res;
+        
+        if (inc >= dec) {
+            res = "Increasing (" + to_string(inc) + "). Decreasing (" + to_string(dec) + ").";
+        } else {
+             res = "Decreasing (" + to_string(dec) + "). Increasing (" + to_string(inc) + ").";
+        }
+        
+        
+        cout << "Case " << k << ". " << res << endl;
+        
     }
 }
---
+++ Vjudge/tp3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int acorn(vector<vector<int>>& arboles, int t, int h, int f) {
+    vector<vector<int>> M(t + 1, vector<int>(h + 1, 0));
+    vector<int> max_per_h(h + 1, 0);
+    
+    for (int i = 1; i <= h; i++) {
+      for (int j = 0; j < t; j++) {
+        int jump = 0;
+        if (i - f >= 0) {
+          jump = max_per_h[i - f];
+        }
+        M[j][i] = arboles[j][i] + max(M[j][i - 1], jump);
+        max_per_h[i] = max(max_per_h[i], M[j][i]);
+      }
+    }
+
+    return max_per_h[h];
+} 
+
+int main() {
+    int casos;
+    cin >> casos;
+
+    for (int caso = 0; caso < casos; caso++) {
+        int t, h, f;
+        cin >> t >> h >> f;
+
+        vector<vector<int>> arboles(t, vector<int>(h + 1, 0));
+
+        for (int i = 0; i < t; i++) {
+          int amount;
+          cin >> amount;
+
+          for (int j = 0; j < amount; j++) {
+            int acorn;
+            cin >> acorn;
+
+            arboles[i][acorn]++;
+          }
+
+        }
+        int acorns = acorn(arboles, t, h, f);
+        printf("%d\n", acorns);
+    }
+}
